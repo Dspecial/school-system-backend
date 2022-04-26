8e46431f451ef0c2b68bc38d2a9028d498214703
Dspecial <1214302256@qq.com>
2022-04-26 14:23:24 +0800

    更新

--- src/core/globalUrl.js
+++ src/core/globalUrl.js
@@ -3,7 +3,7 @@
  * @Email: dxxtalking@163.com
  * @Date: 2021-01-24 16:09:03
  * @LastEditors: dxx
- * @LastEditTime: 2022-04-15 19:02:12
+ * @LastEditTime: 2022-04-26 14:20:33
  */
 
 // const baseURL = 'http://xmgl.ccit.js.cn'
